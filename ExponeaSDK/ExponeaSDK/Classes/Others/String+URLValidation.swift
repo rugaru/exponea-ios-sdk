eaa0b2e530861df2f0e5091941d003fa779026b7
rugaru <rugaru-s@yandex.ru>
2019-09-23 18:37:01 +0300

    Fix webpageURL

--- ExponeaSDK/ExponeaSDK/Classes/Others/String+URLValidation.swift
+++ ExponeaSDK/ExponeaSDK/Classes/Others/String+URLValidation.swift
@@ -10,13 +10,14 @@ import Foundation
 
 extension String {
     var isValidURL: Bool {
-        let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
-        if let match = detector?.firstMatch(in: self, options: [],
-                                            range: NSRange(location: 0, length: utf16.count)) {
-            // it is a link, if the match covers the whole string
-            return match.range.length == utf16.count
-        } else {
-            return false
-        }
+        return self.hasPrefix("http://") || self.hasPrefix("https://")
+//        let detector = try? NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+//        if let match = detector?.firstMatch(in: self, options: [],
+//                                            range: NSRange(location: 0, length: utf16.count)) {
+//            // it is a link, if the match covers the whole string
+//            return match.range.length == utf16.count
+//        } else {
+//            return false
+//        }
     }
 }
